6c6b09312eb1457dad85ada02c7069429fde48a3
frsouzaa <felipe.souza1506@outlook.com>
2024-04-12 23:57:35 -0300

    feat(p1-pdmn): exibe lista real

--- lista-de-tarefas/src/App.js
+++ lista-de-tarefas/src/App.js
@@ -1,14 +1,17 @@
 import LembreteEntrada from "./LembreteEntrada";
 import LembreteLista from "./LembreteLista";
+import { useState } from "react";
 
 function App() {
+  const [lista, setLista] = useState([]);
+
   return (
     <div className="container">
       <div className="row justify-content-center">
         <div className="col-sm-12 col-md-6 col-lg-4">
-          <LembreteLista/>
+          <LembreteLista lista={lista} />
           <br />
-          <LembreteEntrada/>
+          <LembreteEntrada lista={lista} setLista={setLista} />
         </div>
       </div>
     </div>
--- lista-de-tarefas/src/LembreteEntrada.js
+++ lista-de-tarefas/src/LembreteEntrada.js
@@ -1,4 +1,8 @@
-function LembreteEntrada() {
+import { useState } from "react";
+
+function LembreteEntrada(props) {
+  const [lembrete, setLembrete] = useState("");
+
   return (
     <div>
       <input
@@ -6,8 +10,48 @@ function LembreteEntrada() {
         className="form-control text-center mt-3 shadow-none"
         id="novoLembrete"
         placeholder="Digite seu novo lembrete"
+        onChange={(e) => setLembrete(e.target.value)}
+        value={lembrete}
       ></input>
-      <button type="button" className="btn btn-outline-success w-100 mt-3">
+      <button
+        type="button"
+        className="btn btn-outline-success w-100 mt-3"
+        onClick={() => {
+          if (!lembrete) {
+            return;
+          }
+          const cores = [
+            "#f27b7b",
+            "#f2997b",
+            "#f2b77b",
+            "#f2d47b",
+            "#f2f27b",
+            "#d4f27b",
+            "#b7f27b",
+            "#99f27b",
+            "#7bf27b",
+            "#7bf299",
+            "#7bf2b7",
+            "#7bf2d4",
+            "#7bf2f2",
+            "#7bd4f2",
+            "#7bb7f2",
+            "#7b99f2",
+            "#7b7bf2",
+            "#997bf2",
+            "#b77bf2",
+            "#d47bf2",
+            "#f27bf2",
+            "#f27bd4",
+            "#f27bb7",
+            "#f27b99",
+            "#f27b7b",
+          ];
+          const cor = cores[Math.floor(Math.random() * cores.length)];
+          props.setLista([...props.lista, { texto: lembrete, cor: cor }]);
+          setLembrete("");
+        }}
+      >
         OK
       </button>
     </div>
