03e2a920e3c5bfad8718e12ae91a841499242733
Lk-Mota <motalucas802@gmail.com>
2024-04-12 21:15:54 -0300

    feat(p1-pdmn): cria componente principal e faz hello

---
+++ lista-de-tarefas/src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-12">
+          <p>Hello, lembretes</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
